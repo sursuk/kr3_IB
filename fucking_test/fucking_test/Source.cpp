ad29984818799c7a4d61786ef04a0f5ab3116edf
sursuk <sursuk@users.noreply.github.com>
2020-05-05 18:10:29 +0300

    Update Source.cpp

--- fucking_test/fucking_test/Source.cpp
+++ fucking_test/fucking_test/Source.cpp
@@ -54,7 +54,7 @@ bool check_visited_list(bool *visited, int lenght)
 	for (int i = 0; i < lenght; i++)
 	{
 		if (visited[i] == 0)
-			return true; //есть не посещенные точки
+			return true; //пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
 	}
 	return false;
 }
@@ -88,7 +88,7 @@ void output_adjacency_matrix(bool **matrix, int lenght)
 					cout << "  ";
 				else
 					cout << j-1 << " ";
-			}//откудо-то появляется тройка
+			}//пїЅпїЅпїЅпїЅпїЅпїЅ-пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
 			else
 			{
 				if (j == 0)
@@ -207,11 +207,11 @@ void make_bfc_from_matrix(bool **matrix,int lenght)
 	{
 		int start_dotes = *(dotes-=size);
 		if (size != 0)
-			size--;//возможно вот туть size-- уходит в отрицательные значения
+			size--;//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ size-- пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 		for (int i = 0; i <= size - 1; i++, dotes++)
 		{
-			*dotes = *(dotes + 1);//обращайся не по и, а по дотес
-		}//нужно обнулить сайз тут..точнее не обнулить а дополнить, и убрать лишний
+			*dotes = *(dotes + 1);//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅ пїЅ, пїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+		}//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ..пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
 		
 		for (int j = 0; j < lenght; j++)
 		{
@@ -232,7 +232,7 @@ void make_bfc_from_matrix(bool **matrix,int lenght)
 	delete[]dotes;
 }
 
-void find_and_output_return_ribs_in_bfc(bool **matrix,int lenght) //и эта
+void find_and_output_return_ribs_in_bfc(bool **matrix,int lenght) //пїЅ пїЅпїЅпїЅ
 {
 	int* visited_time = new int[lenght] {0};
 	bool* visited = new bool[lenght] {0};
@@ -245,11 +245,11 @@ void find_and_output_return_ribs_in_bfc(bool **matrix,int lenght) //и эта
 	{
 		int start_dotes = *(dotes -= size);
 		if (size != 0)
-			size--;//возможно вот туть size-- уходит в отрицательные значения
+			size--;//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ size-- пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 		for (int i = 0; i <= size - 1; i++, dotes++)
 		{
-			*dotes = *(dotes + 1);//обращайся не по и, а по дотес
-		}//нужно обнулить сайз тут..точнее не обнулить а дополнить, и убрать лишний
+			*dotes = *(dotes + 1);//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅ пїЅ, пїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+		}//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ..пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
 
 		for (int j = 0; j < lenght; j++)
 		{
@@ -282,6 +282,7 @@ void find_and_output_return_ribs_in_bfc(bool **matrix,int lenght) //и эта
 
 	cout << endl;
 
+	delete[]visited_time;
 	delete[]visited;
 	delete[]dotes;
 }
@@ -290,23 +291,23 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int quantity;
-	cout << "Введите кол-во вершин: "; cin >> quantity;
+	cout << "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ-пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ: "; cin >> quantity;
 	bool** matrix = new bool* [quantity];
 	for (int i = 0; i < quantity; i++)
 		matrix[i] = new bool[quantity];
-	cout << "ЕСли вершины соедены введите 1 иначе 0" << endl;
+	cout << "пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ 1 пїЅпїЅпїЅпїЅпїЅ 0" << endl;
 	input_adjacency_matrix(matrix, quantity);
-	cout << "------------матрица смежности-------" << endl;
+	cout << "------------пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ-------" << endl;
 	output_adjacency_matrix(matrix, quantity);
-	cout << "-----------список смежности------------" << endl;
+	cout << "-----------пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ------------" << endl;
 	make_adjacence_list_from_matrix(matrix, quantity);
-	cout << "-----------поиск графа в глубину (dfc)----------" << endl;
+	cout << "-----------пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ (dfc)----------" << endl;
 	make_dfc_from_matrix(matrix, quantity);
-	cout << "-----------поиск в глубину (dfc) вывод обратных ребер----------" << endl;
+	cout << "-----------пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ (dfc) пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ----------" << endl;
 	find_and_output_return_ribs_in_dfc(matrix, quantity);
-	cout << "---------поиск в ширину (bfc)-----------" << endl;
+	cout << "---------пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ (bfc)-----------" << endl;
 	make_bfc_from_matrix(matrix, quantity);
-	cout << "----------поиск в ширину обратные ребра(bfc)-----------" << endl;
+	cout << "----------пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ(bfc)-----------" << endl;
 	find_and_output_return_ribs_in_bfc(matrix, quantity);
 	cout << "----------------------------------" << endl;
 	
